bf63567e307b9f1fd8d6b3c676d31dee505b50ff
recillah khamala <recillahk@gmail.com>
2022-09-24 03:37:57 +0300

    Add the Books component

---
+++ src/component/Books.js
@@ -0,0 +1,35 @@
+import { v4 as uuidv4 } from 'uuid';
+import Book from './Book';
+import Form from './Form';
+
+
+const Books = () => {
+  const bookSamples = [
+    {
+      id: uuidv4(),
+      title: 'The hunger games',
+      author: 'Suzanne Collins',
+    },
+    {
+      id: uuidv4(),
+      title: 'Dune',
+      author: 'Frank Herbert',
+    },
+    {
+      id: uuidv4(),
+      title: 'Capital in the Twenty-First Century',
+      author: 'Suzanne Collins',
+    },
+  ];
+
+  return (
+    <>
+      {bookSamples.map((book) => (
+        <Book key={book.id} title={book.title} author={book.author} />
+      ))}
+      <Form />
+    </>
+  )
+};
+
+export default Books;
