46e1e5b60741aadcb63c0fe060e784fb708ea566
recillah khamala <recillahk@gmail.com>
2022-09-27 17:17:29 +0300

    Define an action type for checking the status

---
+++ src/redux/categories/categories.js
@@ -0,0 +1,12 @@
+const CHECK_STATUS = redux/books/Categories/CHECK_STATUS;
+
+const initialState = [];
+
+const checkBookStatus = () => ({ type: CHECK_STATUS })
+
+const chechReducer = (state = [], action){
+  switch(action.type){
+    case CHECK_STATUS:
+      return 
+  }
+}
