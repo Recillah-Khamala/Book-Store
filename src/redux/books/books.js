15a2206bd7fb08fef10abd4be3a3a31b8d497da0
recillah khamala <recillahk@gmail.com>
2022-09-27 15:56:57 +0300

    Export Action Creators

--- src/redux/books/books.js
+++ src/redux/books/books.js
@@ -2,3 +2,16 @@ const ADD_BOOK = 'redux/books/book/ADD_BOOK';
 const REMOVE_BOOK = 'redux/books/book/REMOVE_BOOK';
 
 const initialState = [];
+
+const newBook = (id, title, author) => ({
+  type: 'ADD_BOOK',
+  title,
+  author,
+});
+
+const removeBook = (id) => ({
+  type: 'REMOVE_BOOK',
+  id,
+})
+
+export default { newBook, removeBook} 
