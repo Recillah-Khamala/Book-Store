d09f82973ae0f391f0d19e36819c46721b06c233
recillah khamala <recillahk@gmail.com>
2022-09-27 17:47:56 +0300

    Combine both reducers into a root reducer by using configureStore function

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -2,3 +2,11 @@ import { combineReducers, configureStore } from "@reduxjs/toolkit";
 import bookReducer from './books/books';
 import checkReducer from './categories/categories';
 
+const store = combineReducers({
+  reducer: {
+    books: bookReducer,
+    categories: checkReducer,
+  }
+})
+
+export default store;
